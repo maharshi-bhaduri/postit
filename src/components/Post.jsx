6120dbc6d692581d6668959e8800e9c36d46c6be
Maharshi Bhaduri <bhaduri.maharshi@gmail.com>
2022-04-20 00:10:06 +0530

    increasing retention time to 1 hour

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,7 +5,7 @@ function Post(props) {
   time = time < 0 ? 0 : time
   var timeInMins = String(Math.floor(time/60)) + 'm ago'
   // var postColor = time < 1500 ? (time < 300 ? {backgroundColor:'#d7d7ff'} : null) : {backgroundColor:'#ffbbbb'}
-  var textColor = time < 1500 ? (time < 300 ? {color:'#1000ff'} : null) : {color:'#ff0000'}
+  var textColor = time < 3600 ? (time < 300 ? {color:'#1000ff'} : null) : {color:'#ff0000'}
 
   return (
     <div className="post-card" style={null}>
