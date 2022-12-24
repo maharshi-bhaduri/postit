9e8a0d42717b2f971a17f3897d9f0a24a4d40722
Maharshi Bhaduri <bhaduri.maharshi@gmail.com>
2022-12-24 19:41:35 +0530

    fixing the navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,9 +3,20 @@ import React from "react";
 
 function NavBar() {
     const logo = require('../img/logo_b.png');
+    const addPost = require('../img/add_post.png');
+    const addPostHover = require('../img/add_post_hover.png');
+    const addPostClick = require('../img/add_post_click.png');
     return (
         <div className="nav-container">
-            <img src={logo} className="logo"></img>
+            <img src={logo} className="logo" draggable="false"></img>
+            <img src={addPost} className="add-post"
+            onMouseOver={e => (e.currentTarget.src = addPostHover)}
+            onMouseLeave={e => (e.currentTarget.src = addPost)}
+            onMouseDown={e => (e.currentTarget.src = addPostClick)}
+            onMouseUp={e => (e.currentTarget.src = addPostHover)}
+                draggable="false"
+            ></img>
+            
         </div>
     );
 }
